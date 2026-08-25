cc9971261b05cce1a0647e1b758dc77fc43d9cd3
karthirock98 <karthirock98@gmail.com>
2026-08-25 15:21:47 +0530

    Basic Login Page created and Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+import 'altcha';
+
 
 function App() {
 
   return (
     <>
-    
+      <div className='flex gap-4 h-screen justify-between'>
+        <div className='flex flex-col items-center justify-center w-full'>
+          <div className="card shadow">
+            <Input placeholder='Username' />
+
+            <Button variant="default">Login</Button>
+          </div>
+        </div>
+        <div className='flex flex-col items-center w-full justify-center'>
+          <span>LAYOUT 2</span>
+        </div>
+      </div>
     </>
   )
 }
