d900fb336840c69054995dea71dce9d4dcdb8f7e
Gayatri Shetty <gayatri.j.shetty@gmail.com>
2026-08-27 07:10:00 +0000

    angular client created

--- server/src/utils/common-utils.js
+++ server/src/utils/common-utils.js
@@ -1,8 +1,14 @@
-export const createResponse = (statusCode = 500, data = null, message = 'Something Went Wrong!', meta = null, other) => {
-    return {
-        statusCode: statusCode,
-        data: data,
-        message: message,
-        meta: meta
-    }
-}
+export const createResponse = (
+  statusCode = 500,
+  data = null,
+  message = "Something Went Wrong!",
+  meta = null,
+  other,
+) => {
+  return {
+    statusCode: statusCode,
+    data: data,
+    message: message,
+    meta: meta,
+  };
+};
