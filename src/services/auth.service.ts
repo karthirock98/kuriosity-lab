8e6af556d751f8db30da573c2ca69c6452d8bfb3
karthirock98 <karthirock98@gmail.com>
2026-09-16 15:32:20 +0530

    fix

---
+++ src/services/auth.service.ts
@@ -0,0 +1,15 @@
+import axios from "axios"
+const BASE_URL = "http://localhost:3333"
+
+export const loginUser = async (payload: any) => {
+    try {
+        const res = await axios.post(
+            `${BASE_URL}/auth/login`,
+            payload
+        );
+
+        return res;
+    } catch (error) {
+        throw error;
+    }
+};
